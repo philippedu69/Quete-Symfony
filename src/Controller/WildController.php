3973b223325bdd03a4ee2e8db2e32ee8bf1aba34
philippedu69 <philippeperrin1968@free.fr>
2019-12-02 18:02:22 +0100

    quete Symfony5 : le routing avancé

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,16 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
 
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("", name="index")
     */
     public function index() :Response
     {
-        return $this->render('wild/index.html.twig', [
+        return $this->render('/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/show/{slug}",
+     * requirements={"slug"="[a-z0-9-]+$"},
+     * defaults={"slug"="", "Aucune série sélectionnée, veuillez choisir une série"}, name="show")
+     */
+    public function show(string $slug) :Response
+    {
+        $slug = str_replace("-", " ", $slug);
+        return $this->render('wild/show.html.twig', ['slug' => ucwords($slug)]);
     }
 }
